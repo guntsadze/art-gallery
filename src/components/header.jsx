890fed6dbb3e7047af4d2c56816fbf5824d79a67
guntsadze <dimitri.guncadze@gmail.com>
2023-02-06 23:22:33 +0400

    sass

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,28 +2,34 @@ import { F } from "./imgs"
 
 export default function RenderHeader() {
     
-    return (
+    return (   
         <div className="header">
         <div className="header-background">
-          <div className="black-background"></div>
-          <img className="bg-img" src={F.image0} />
-          <h1 className="header-title">
-            MODERN <br /> ART GALLERY
-          </h1>
-        </div>
-        <div className="location-btn">
+          <div className="black-background">
+          <div className="location-btn">
           <a href={"location"} className="location-div">
             OUR LOCATION
           </a>
           <div className="arrow-right">
             <img src={F.arrowRight} />
           </div>
-        </div>
-        <p>
+          
+          </div>
+          <h1 className="header-title">
+            MODERN <br /> ART GALLERY
+          </h1>
+          <p>
           The arts in the collection of the Modern Art Gallery all started from
           a spark of inspiration. Will these pieces inspire you? Visit us and
           find out.
-        </p>
-      </div>
+         </p>
+          </div>
+          <div>
+          <img className="bg-img" src={F.image0} />
+          
+          
+        </div>
+        </div>
+        </div>
     )
 }
